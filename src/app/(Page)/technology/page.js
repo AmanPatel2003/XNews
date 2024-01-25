0a2b5e18289fe819eb268b9c3191fa2378af16f2
aman <amanpatel22012003@gmail.com>
2024-01-25 18:25:14 +0530

    commit

---
+++ src/app/(Page)/technology/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const metadata = {
+  title: "Technology page",
+  description: "Technology page description",
+};
+
+const page = () => {
+ return (
+   <>
+     <div className="">
+       <div className="mt-[75px] h-[80vh] flex justify-center items-center">
+         <h1 className="text-6xl text-gray-500">Coming soon!</h1>
+       </div>
+     </div>
+   </>
+ );
+}
+
+export default page
