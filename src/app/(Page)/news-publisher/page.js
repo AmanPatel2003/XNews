646e527737c0fd787789f9565d3cd5e0048a8eeb
aman <amanpatel22012003@gmail.com>
2024-01-25 18:17:06 +0530

    commit

---
+++ src/app/(Page)/news-publisher/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const metadata = {
+  title: "News Publishers page",
+  description: "News Publishers page description",
+};
+
+const page = () => {
+   return (
+     <>
+       <div className="">
+         <div className="mt-[75px] h-[80vh] flex justify-center items-center">
+           <h1 className="text-6xl text-gray-500">Coming soon!</h1>
+         </div>
+       </div>
+     </>
+   );
+}
+
+export default page
