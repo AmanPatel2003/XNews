f02db5790b6ddd4fa976dc5a8c16e194e0024422
aman <amanpatel22012003@gmail.com>
2024-01-25 18:15:10 +0530

    commit

---
+++ src/app/(Page)/leadership/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const metadata = {
+  title: "Leadership page",
+  description: "Leadership page description",
+};
+
+const page = () => {
+   return (
+     <>
+       <div className="">
+         <div className="mt-[75px] h-[80vh] flex justify-center items-center">
+           <h1 className="text-6xl text-gray-500">Coming soon!</h1>
+         </div>
+       </div>
+     </>
+   );
+}
+
+export default page
